3f1db2f09b7e17c99c8668e68545ef2c89c3349e
Sarath Maddineni <sarathmaddineni07@gmail.com>
2020-04-11 12:40:47 -0400

    Client Server Communication

---
+++ server/models/Users.js
@@ -0,0 +1,19 @@
+const Users = [
+    { Name: 'Moshe', Password: '2020', Email: 'plotkinm@newpaltz.edu', userId: 0 },
+    { Name: 'Bracha', Password: '5780', Email: 'chabad@newpaltz.edu', userId: 1},
+    
+];
+
+module.exports = {
+    Login(email, password) {
+    
+        const user = Users.find(x => x.Email == email);
+        if(!user) throw Error('User not found');
+        if(user.Password != password) throw Error('Wrong Password');
+    
+        return user;
+    },
+    Get(userId) {
+        return Users[userId]
+    }
+}
